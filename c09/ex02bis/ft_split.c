60e9e1a0d5bfc5e393f2a73ba6631b2e8ce16d4f
Saiban Bern <bartched@outlook.fr>
2023-10-10 19:20:33 +0200

    progres ft_split

--- c09/ex02bis/ft_split.c
+++ c09/ex02bis/ft_split.c
@@ -1,6 +1,37 @@
-#include<unistd.h>
-#include<stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 18:59:22 by bchedru           #+#    #+#             */
+/*   Updated: 2023/10/10 19:19:55 by ubuntu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int	count_words(char *str, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (str)
+	{
+		/* code */
+	}
+	
+
+}
+
+char	**ft_split(char *str, char *charset)
+{
+
+}
 
 int	main(void)
 {
