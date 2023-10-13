36bb73fceaf8d6c4d3eb78a8ce1e5d89e62485ba
Saiban Bern <bartched@outlook.fr>
2023-10-13 21:42:43 +0200

    coucou c11

---
+++ c11/ex00/ft_foreach.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+
+void    ft_foreach(int *tab, int length, void(*f)(int))
+{
+        /*wallah je bosserai quand j'aurai pas la flemme*/
+}
+
+int     main(void)
+{
+        return(0);        
+}
