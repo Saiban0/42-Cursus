21deb451732abb9f2f6434162959bc552db5eaa6
Saiban Bern <bartched@outlook.fr>
2023-10-22 23:09:22 +0200

    ft_strdup

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 23:00:33 by bchedru           #+#    #+#             */
-/*   Updated: 2023/10/21 12:10:32 by bchedru          ###   ########.fr       */
+/*   Updated: 2023/10/22 23:09:04 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ int			ft_memcmp(const void *s1, const void *s2, size_t n);
 char		*ft_strnstr(const char *big, const char *little, size_t len);
 int			ft_atoi(const char *nb);
 void		*ft_calloc(size_t nmemb, size_t size);
+char		*ft_strdup(const char *s);
 
 #endif
