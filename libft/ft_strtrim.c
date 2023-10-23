4633c2689a9566dd54dead8de76a7f692ad1f33b
Saiban Bern <bartched@outlook.fr>
2023-10-23 13:59:46 +0200

    je decouvre des chose sur ce que j'ai cree

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: bchedru <bchedru@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:24:15 by bchedru           #+#    #+#             */
-/*   Updated: 2023/10/23 13:08:25 by bchedru          ###   ########.fr       */
+/*   Updated: 2023/10/23 13:59:07 by bchedru          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int in_set(const char s, const char *set)
+static int	in_set(const char s, const char *set)
 {
 	int	i;
 
@@ -33,22 +33,22 @@ char	*ft_strtrim(const char *s1, const char *set)
 
 	if (!s1 || !set)
 		return (NULL);
-	size = ft_strlen(s1);
-	while(s1)
+	while (s1)
 	{
-		if (in_set(((char *)s1)[size], set) == 1)
+		if (in_set(((char )*s1), set) == 1)
 			s1++;
 		else
-			break;
+			break ;
 	}
+	size = ft_strlen(s1);
 	while (size != 0)
 	{
-		if(in_set(s1[size - 1], set) == 1)
+		if (in_set(s1[size - 1], set) == 1)
 			size--;
 		else
-			break;
+			break ;
 	}
-	res = (char *)malloc(size + 1 * sizeof(char));
+	res = (char *)malloc(size * sizeof(char) + 1);
 	if (!res)
 		return (NULL);
 	ft_strlcpy(res, (char *)s1, size + 1);
